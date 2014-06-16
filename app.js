f872d13dee1ef82f23487f70a9c98106657d0d32
rucka <miroslav.rucka@posam.sk>
2014-06-16 21:53:51 +0200

    express showcase

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-fs = require('fs');
-var start = new Date().getTime();
-var data1 = fs.readFileSync('grid.png');
-console.log('data1 ', data1);
-var data2 = fs.readFileSync('grid.png');
-console.log('data', data2);
-console.log('do something else ', 'duration', new Date().getTime() - start, 'ms');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.get('/pads', function(req, res){
+    res.send('Hello World Pads!');
+});
+
+app.listen(8080);
+console.log('express started > port: 8080');
