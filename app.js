cc45cec74947badecfd7537690db78aa63f9cd30
rucka <miro.rucka@gmail.com>
2014-06-16 16:03:37 +0200

    blocking vs nonblocking

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 fs = require('fs');
 var start = new Date().getTime();
-var data1 = fs.readFileSync('angular.zip');
+var data1 = fs.readFileSync('d:\\booksJava\\clean-code-01-4-metody-dekompozicia.avi');
 console.log('data1 ', data1);
 console.log('data1 ', 'duration', new Date().getTime() - start, 'ms');
-var data2 = fs.readFileSync('angular.zip');
+var data2 = fs.readFileSync('d:\\booksJava\\clean-code-01-4-metody-dekompozicia.avi');
 console.log('data', data2);
 console.log('data2 ', 'duration', new Date().getTime() - start, 'ms');
 console.log('do something else ', 'duration', new Date().getTime() - start, 'ms');
