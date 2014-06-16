000d3f6802f6899afee44c9b8d84fbe8be5ee423
rucka <miro.rucka@gmail.com>
2014-06-16 16:09:13 +0200

    blocking vs nonblocking

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 fs = require('fs');
 var start = new Date().getTime();
-fs.readFile('angular.zip', function (err,data) {
+fs.readFile('grid.png', function (err,data) {
     console.log('data1 ', data);
-    console.log('data1 ', 'duration', new Date().getTime() - start, 'ms');
 });
-fs.readFile('angular.zip', function (err,data) {
+fs.readFile('grid.png', function (err,data) {
     console.log('data', data);
-    console.log('data2 ', 'duration', new Date().getTime() - start, 'ms');
 });
 console.log('do something else ', 'duration', new Date().getTime() - start, 'ms');
