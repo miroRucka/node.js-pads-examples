fd22ef698de7a97b2cf086fe5fe65baad840cd02
rucka <miro.rucka@gmail.com>
2014-06-17 16:28:05 +0200

    example express 4

--- public/js/pads.js
+++ public/js/pads.js
@@ -1,1 +1,30 @@
-console.log('test js!');
+(function(w){
+    w.pads = function(){
+        var _renderTemplate = function(id, data){
+            var source = $(id).html();
+            var template = Handlebars.compile(source);
+            $('div.table').append(template(data));
+        };
+        var _getData = function(cb) {
+            $.ajax({
+                dataType: "json",
+                url: '/api',
+                success: function(data){
+                    cb(data);
+                }
+            });
+        };
+        return {
+            render: _renderTemplate,
+            data: _getData
+        }
+    };
+}(window));
+
+var pads = pads();
+pads.data(function(data){
+    var users = {
+        users: data
+    };
+    pads.render("#table", users);
+});
