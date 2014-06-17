558c35cad5295bc1049d6fcb3f44aaba2616578a
rucka <miro.rucka@gmail.com>
2014-06-17 15:43:45 +0200

    example express 2, reorganizing folders and register javascript, css

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
+app.use('/bower_components', express.static('bower_components'));
+
 
 app.get('/', function (req, res) {
-    res.sendfile('index.html');
+    res.sendfile('views/index.html');
 });
 
 app.get('/api', function(req, res){
-    var data = require('./data.json');
+    var data = require('./data/data.json');
     res.json(data);
 });
 
